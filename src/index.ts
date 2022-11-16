411e5623564decc87fa07d6786702150aabf8317
Samuele Girgenti <samuele.girgenti458@gmail.com>
2022-11-16 12:34:20 +0100

    Release 1.0.21

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) Minteeble 2022. All Rights Reserved.
- * Node module: @minteeble/sdk
+ * Node module: @minteeble/sdk.
  * This file is licensed under the MIT License.
  * License text available at https://opensource.org/licenses/MIT
  *
