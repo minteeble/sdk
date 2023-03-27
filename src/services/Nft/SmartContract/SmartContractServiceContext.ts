07cc4b3dfa0820d61a34009930497a9349d5a1b8
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-03-27 09:20:42 +0200

    update

--- src/services/Nft/SmartContract/SmartContractServiceContext.ts
+++ src/services/Nft/SmartContract/SmartContractServiceContext.ts
@@ -1,7 +1,16 @@
 import { createContext } from "react";
 import React from "react";
+import { SmartContractInstance } from "./SmartContractInstance";
 
-export interface SmartContractServiceContent {}
+export interface SmartContractServiceContent {
+  getSmartContractInstance: (
+    chainName: string,
+    id: string,
+    connect: boolean
+  ) => Promise<SmartContractInstance | null>;
+}
 
 export const SmartContractServiceContext =
-  createContext<SmartContractServiceContent>({});
+  createContext<SmartContractServiceContent>({
+    getSmartContractInstance: () => new Promise(() => {}),
+  });
--- src/services/Nft/SmartContract/SmartContractServiceProvider.tsx
+++ src/services/Nft/SmartContract/SmartContractServiceProvider.tsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useWalletService } from "../../WalletService";
+import { SmartContractInstance } from "./SmartContractInstance";
+import { SmartContractService } from "./SmartContractService";
 import { SmartContractServiceProviderProps } from "./SmartContractService.types";
 import { SmartContractServiceContext } from "./SmartContractServiceContext";
 
 export const SmartContractServiceProvider = (
   props: SmartContractServiceProviderProps
 ) => {
+  let { walletAddress, web3 } = useWalletService();
+
+  const getSmartContractInstance = async (
+    chainName: string,
+    id: string,
+    connect: boolean
+  ): Promise<SmartContractInstance | null> => {
+    return new Promise<SmartContractInstance | null>(
+      async (resolve, reject) => {
+        try {
+          if (connect && !web3) {
+            reject("Error. Client is not yet connected to web3.");
+            return;
+          }
+
+          let smartContractInfo =
+            await SmartContractService.instance.getSmartContractInfo(
+              chainName,
+              id
+            );
+
+          let smartContractInstance: SmartContractInstance | null =
+            new SmartContractInstance(smartContractInfo || undefined, web3);
+
+          resolve(smartContractInstance);
+        } catch (err) {
+          console.log(err);
+          reject(err);
+        }
+      }
+    );
+  };
+
   return (
-    <SmartContractServiceContext.Provider value={{}}>
+    <SmartContractServiceContext.Provider value={{ getSmartContractInstance }}>
       {props.children}
     </SmartContractServiceContext.Provider>
   );
