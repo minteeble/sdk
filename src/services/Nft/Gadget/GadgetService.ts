d78f65764ab76ef12c7cf5dedb2b30281f19b393
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2023-04-30 16:48:27 +0200

    update

--- src/services/Nft/Gadget/GadgetService.ts
+++ src/services/Nft/Gadget/GadgetService.ts
@@ -104,12 +104,6 @@ export class GadgetService extends BaseService {
     tokenId: string,
     imageString: string
   ): Promise<void> {
-    let body = new Blob([Buffer.from(imageString, "base64")], {
-      type: "image/png",
-    });
-
-    console.log("BLOB:", body.size, imageString.length);
-
     await this.apiCaller.post(
       `/group/${groupId}/token/${tokenId}`,
       {
