253e0431fdae3453878df68c6ca46dcd941689bd
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-05-10 02:15:02 +0200

    Release 1.0.47

--- src/services/ReservedAreaService/index.ts
+++
@@ -1,6 +0,0 @@
-import ReservedAreaService from "./ReservedAreaService";
-
-export { ReservedAreaService };
-export * from "./ReservedAreaServiceContext";
-export * from "./ReservedAreaServiceProvider";
-export * from "./useReservedAreaService";
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,5 @@
 export * from "./WalletService";
 export * from "./AuthService";
-export * from "./ReservedAreaService";
 export * from "./NavigationService";
 export * from "./Nft";
 export * from "./Shared";
