4589fdc230a880dc15cb0a3552e991613b6d85f4
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2023-05-20 10:32:17 +0200

    shortener gets api

--- src/services/Shortener/ShortenerService.ts
+++ src/services/Shortener/ShortenerService.ts
@@ -1,14 +1,21 @@
 import {
   CreateShortenedRequestDto,
   CreateShortenerRequestDto,
+  ICreateShortenedRequestDto,
   ICreateShortenerRequestDto,
+  ShortenedClientModel,
   ShortenedType,
+  ShortenerClientModel,
+  ShortenerPreviewClientModel,
   ShorteningType,
 } from "@minteeble/utils";
 import { BaseService } from "../../shared/BaseService";
+import { JsonSerializer } from "typescript-json-serializer";
+
+const serializer = new JsonSerializer();
 
 /**
- * Singleton class for handling Shortnere service features
+ * Singleton class for handling Shortner service features
  */
 class ShortenerService extends BaseService {
   /**
@@ -55,6 +62,82 @@ class ShortenerService extends BaseService {
     if (res && res.id && typeof res.id === "string") {
       return res.id;
     } else throw new Error("Fail on creating shortener.");
+  }
+
+  public async createShortened(
+    shortenerId: string,
+    type: ShortenedType,
+    object: any
+  ): Promise<string> {
+    let body: ICreateShortenedRequestDto = {
+      shortenerId,
+      type,
+      object,
+    };
+
+    let reqInit: any = {
+      responseType: "text",
+      body,
+    };
+
+    let res = await this.apiCaller.post(`/shortened`, reqInit);
+
+    if (res && res.id && typeof res.id === "string") {
+      return res.id;
+    } else throw new Error("Fail on creating shortened.");
+  }
+
+  public async getShortened(
+    shortenerId: string,
+    shortenedObjectId: string
+  ): Promise<ShortenedClientModel | null> {
+    let data = await this.apiCaller.get(
+      `/shortener/${shortenerId}/shortened/${shortenedObjectId}/shortened`,
+      {},
+      true
+    );
+
+    let shortened = serializer.deserializeObject<ShortenedClientModel>(
+      data,
+      ShortenedClientModel
+    );
+
+    return shortened || null;
+  }
+
+  public async getShortener(
+    shortenerId: string
+  ): Promise<ShortenerClientModel | null> {
+    let data = await this.apiCaller.get(
+      `/shortener/${shortenerId}/shortener`,
+      {},
+      true
+    );
+
+    let shortener = serializer.deserializeObject<ShortenerClientModel>(
+      data,
+      ShortenerClientModel
+    );
+
+    return shortener || null;
+  }
+
+  public async getShorteners(
+    shortenerId: string
+  ): Promise<ShortenerPreviewClientModel[] | null> {
+    let data = await this.apiCaller.get(
+      `/shortener/${shortenerId}/shortener`,
+      {},
+      true
+    );
+
+    let shorteners =
+      (serializer.deserializeObjectArray<ShortenerPreviewClientModel>(
+        data.shorteners,
+        ShortenerPreviewClientModel
+      ) || []) as [];
+
+    return shorteners || null;
   }
 }
 
