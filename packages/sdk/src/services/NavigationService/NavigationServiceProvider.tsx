f7cc0b1aff91e825624456be3a30021d269efa64
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-10-13 00:10:31 +0300

    Init navigation service

--- packages/sdk/src/services/NavigationService/NavigationServiceProvider.tsx
+++ packages/sdk/src/services/NavigationService/NavigationServiceProvider.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import NavigationService from "./NavigationService";
-import { UserNavigationInfo } from "./NavigationService.types";
 import { NavigationServiceContext } from "./NavigationServiceContext";
 import React from "react";
 
@@ -11,42 +10,8 @@ export interface NavigationServiceProviderProps {
 export const NavigationServiceProvider = (
   props: NavigationServiceProviderProps
 ) => {
-  const [navigationService, setNavigationService] =
-    useState<NavigationService | null>(null);
-
-  const [userNavigationInfo, setUserNavigationInfo] =
-    useState<UserNavigationInfo | null>(null);
-
-  useEffect(() => {
-    let service = new NavigationService();
-
-    setNavigationService(service);
-  }, []);
-
-  const loadUserNavigationInfo = async (): Promise<void> => {
-    return new Promise<void>(async (resolve, reject) => {
-      try {
-        console.log("Navigation service:", navigationService);
-        if (navigationService) {
-          let navigationInfo =
-            await navigationService.getNavigationInfoForUser();
-
-          setUserNavigationInfo(navigationInfo);
-
-          resolve();
-        }
-      } catch (err) {
-        setUserNavigationInfo(null);
-        console.log(err);
-        reject(err);
-      }
-    });
-  };
-
   return (
-    <NavigationServiceContext.Provider
-      value={{ navigationService, userNavigationInfo, loadUserNavigationInfo }}
-    >
+    <NavigationServiceContext.Provider value={{}}>
       {props.children}
     </NavigationServiceContext.Provider>
   );
