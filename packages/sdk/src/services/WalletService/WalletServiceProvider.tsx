c204b90e07d55f3ecfe975aa19681fc243efda8c
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-10-13 00:18:09 +0300

    Alchemy

--- packages/sdk/src/services/WalletService/WalletServiceProvider.tsx
+++ packages/sdk/src/services/WalletService/WalletServiceProvider.tsx
@@ -7,21 +7,34 @@ import {
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 import { InjectedConnector } from "wagmi/connectors/injected";
 
 export interface WalletServiceProviderProps
   extends WalletServiceProviderContentProps {
   appName?: string;
   walletConnectProjectId: string;
+  alchemyApiKey?: string;
   chains: Array<any>;
 
   children: any;
 }
 
 export const WalletServiceProvider = (props: WalletServiceProviderProps) => {
-  const { chains, publicClient } = configureChains(props.chains || [], [
-    publicProvider(),
-  ]);
+  let providers = [publicProvider()];
+
+  if (props.alchemyApiKey) {
+    providers.push(
+      alchemyProvider({
+        apiKey: props.alchemyApiKey,
+      })
+    );
+  }
+
+  const { chains, publicClient } = configureChains(
+    props.chains || [],
+    providers
+  );
 
   const { connectors } = getDefaultWallets({
     appName: props.appName || "Minteeble Sdk Consumer",
