7f24da4e87a80de1d1970d489f1c2ed7cd3ffc89
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-11-04 18:16:15 +0100

    Fix deserialization

--- packages/sdk/src/services/Renderer/RendererService.ts
+++ packages/sdk/src/services/Renderer/RendererService.ts
@@ -331,15 +331,11 @@ export class RendererService extends BaseService {
       false
     );
 
-    console.log("res", res);
-
     let itemsInfo: NftGenerationItemInfoClientModel[] =
       (serializer.deserializeObjectArray<NftGenerationItemInfoClientModel>(
-        res.itemsInfo,
+        JSON.parse(res).itemsInfo,
         NftGenerationItemInfoClientModel
       ) || []) as NftGenerationItemInfoClientModel[];
-
-    console.log("itemsInfo", itemsInfo);
 
     return itemsInfo;
   }
